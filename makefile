6cb43b482b746e968b56c761dddbe330f3404a27
noro <norbert.durcansky@gmail.com>
2014-10-04 15:50:32 +0200

    Signed-off-by: noro <norbert.durcansky@gmail.com>

--- makefile
+++ makefile
@@ -3,8 +3,8 @@ BIN=project
 CC=gcc
 RM=rm -f
 
-ALL: scaner.o main.o
-	$(CC) $(CFLAGS) -o $(BIN) scaner.o main.o
+ALL: scaner.o parser.o main.o
+	$(CC) $(CFLAGS) -o $(BIN) scaner.o parser.o main.o
 
 clean:
 	$(RM) *.o $(BIN)
---
+++ parser.c
@@ -0,0 +1,19 @@
+/* **************************** parser.c ************************************ */
+/* Soubor:              parser.c - Syntakticka a Semanticka analyza           */
+/* Kodovani:            UTF-8                                                 */
+/* Datum:               11.2014                                               */
+/* Predmet:             Formalni jazyky a prekladace (IFJ)                    */
+/* Projekt:             Implementace interpretu jazyka IFJ14                  */
+/* Varianta zadani:     a/2/II                                                */
+/* Titul,Autori, login:         Ing. Natalya Loginova   xlogin00              */
+/*                              Jindrich Dudek          xdudek04              */
+/*                              Norbert Durcansky       xdurca01              */
+/*                              Jan Jusko               xjusko00              */
+/*                              Jiøí Dostál             xdosta40              */
+/* ****************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include "scaner.h"
+#include "parser.h"
+
+
