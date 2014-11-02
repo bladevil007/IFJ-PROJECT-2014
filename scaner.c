c73ecda3ac70bd9dd3377f8d8485c77bf4095584
noro <norbert.durcansky@gmail.com>
2014-11-02 16:28:46 +0100

    Signed-off-by: noro <norbert.durcansky@gmail.com>

--- scaner.c
+++ scaner.c
@@ -14,7 +14,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <strings.h>
 #include <ctype.h>
 #include <math.h>
 #include "parser.h"
--- string.c
+++ string.c
@@ -14,7 +14,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <strings.h>
 #include <math.h>
 #include <ctype.h>
 #include "string.h"
