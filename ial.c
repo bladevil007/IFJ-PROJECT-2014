91a7052811cad610591f2942d0a3d68b4b49d61e
noro <norbert.durcansky@gmail.com>
2014-11-19 21:41:38 +0100

    HASH SPOJENA S IAL.C Signed-off-by: noro <norbert.durcansky@gmail.com>

--- ial.c
+++ ial.c
@@ -11,9 +11,119 @@
 /*                              Jan Jusko               xjusko00              */
 /*                              Jiri Dostal             xdosta40              */
 /* ****************************************************************************/
+#include <strings.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <strings.h>
+#include <string.h>
+#include <ctype.h>
+#include "ial.h"
+
+///Jindrova Hash tabulka
+void stringtolower (char *key) //funkce ktera prevede vsechny znaky v retezci na male znaky
+{
+	int i = 0;
+	while (key[i] != '\0')
+	{
+		key[i] = tolower(key[i]);
+		i++;
+	}
+}
+
+THash_table *hashtable_init (unsigned size) //funkce na inicializaci tabulky
+{
+	THash_table *tmp = NULL; //docasna promenna, do ktere se ulozi ukazatel na tabulku
+
+	if ((tmp = malloc(sizeof(THash_table) + (sizeof(struct record *)*size))) == NULL)  // alokace pameti pro polozku hashtable_size ve strukture + size*velikost ukazatele na zaznam slova
+	{
+		fprintf(stderr, "Allocation error.\n");
+		return NULL;
+	}
+	tmp->hashtable_size = size; //ulozeni poctu polozek pole do struktury
+	for(unsigned i = 0; i < tmp->hashtable_size; i++) //jednotlive polozky pole se vyplni NULL pointerama
+		tmp->ptr[i] = NULL;
+	return tmp;
+}
+
+unsigned int hash_function (const char *str, unsigned hashtable_size)  //hashovaci funkce
+{
+	unsigned int h=0;
+	const unsigned char *p;
+
+	for(p=(const unsigned char*)str; *p!='\0'; p++)
+		h = 65599*h + *p;
+	return h % hashtable_size;
+}
+
+void hashtable_clear (THash_table *table) //funkce, ktera uvolni vsechny zaznamy v tabulce
+{
+	for (unsigned i = 0; i < (table->hashtable_size); i++) //prochazeni vsech radku v tabulce
+	{
+		while (table->ptr[i] != NULL) //postupne uvolnovani linearniho seznamu od prvniho zaznamu
+		{
+			struct record *tmp;
+			tmp = table->ptr[i];
+			table->ptr[i] = tmp->next;
+			if (tmp->params != NULL) //pokud se jedna o funkci a retezec params neni prazdny
+			{
+				free(tmp->params);
+			}
+			if (tmp->id == VARIABLE && tmp->type == STRING && tmp->defined == true) //pokud je zaznam promenna typu string a ma prirazenou hodnotu je treba ji uvolnit
+			{
+				free(tmp->value.str);
+			}
+			free(tmp);
+		}
+	}
+	return;
+}
+
+void hashtable_free(THash_table *table) //funkce na uvolneni cele tabulky
+{
+	hashtable_clear(table);
+	free(table);
+	return;
+}
+
+struct record *hashtable_search(THash_table *table, char *key) //funkce na vyhledani zaznamu
+{
+	unsigned int index = 0; //promenna, do ktere se ulozi index z hashovaci funkce
+	stringtolower(key); //prevedeni retezce na mala pismena
+	index = hash_function(key, table->hashtable_size); //zjisteni indexu v tabulce pro ulozeni zaznamu
+	for (struct record *tmp = table->ptr[index]; tmp != NULL; tmp = tmp->next) //projizdeni jednotlivych zaznamu linearniho seznamu
+	{
+		if (strcmp(tmp->key, key) == 0) //porovna se hledane slovo se slovem ve strukture
+		{
+			return tmp;
+		}
+	}
+	//pokud se program dostane do tohoto mista, znamena to, ze nebyla nalezena shoda se slovem:
+	return NULL;
+}
+
+struct record *hashtable_add(THash_table *table, int id, char *key, int type, char *params) //funkce pro pridani zaznamu do tabulky
+{
+	unsigned int index = 0; //promenna, do ktere se ulozi index z hashovaci funkce
+	stringtolower(key); //prevedeni retezce na mala pismena
+	index = hash_function(key, table->hashtable_size); //zjisteni indexu v tabulce pro ulozeni zaznamu
+
+	struct record *tmp; //pomocna promenna
+	if ((tmp = malloc(sizeof(struct record))) == NULL) //pokud nelze alokovat pamet
+	{
+		fprintf(stderr, "Allocation error.\n");
+		return NULL;
+	}
+	//pripojeni noveho zaznamu do seznamu
+	tmp->next = table->ptr[index];
+	table->ptr[index] = tmp;
+	tmp->id = id; //pridani informaci do zaznamu
+	tmp->key = key;
+	tmp->type = type;
+	tmp->params = params;
+	tmp->defined = false;
+	return tmp;
+}
+
+///Nataly funkcie
 
 /* calculation of string length */
 int length(char *s) {
@@ -53,7 +163,7 @@ void failKMP(char *p, int pl, int *fail) {
 }
 
 /* KMP algorithm */
-int matchKMP(char *search, char *txt, int pl, int tl, int *fail) {
+/*int matchKMP(char *search, char *txt, int pl, int tl, int *fail) {
 
 	int pi, ti;
 
@@ -74,10 +184,10 @@ int matchKMP(char *search, char *txt, int pl, int tl, int *fail) {
 		else
 			matchKMP = ti;
 	}
-}
+}*/
 
 /* substring search */
-int find(char *s, char *search) {
+/*int find(char *s, char *search) {
 
 	int pl, tl, result;
 
@@ -97,7 +207,7 @@ int find(char *s, char *search) {
 	else
 		return result;
 }
-
+*/
 /* sift down procedure for heap sort algorithm */
 void siftDown(char *s, int left, int right) {
 
--- ial.h
+++ ial.h
@@ -1,22 +1,55 @@
-/* ****************************   ial.h  ************************************ */
-/* Soubor:              ial.h - Hlavickovy soubor pro ial.c (Vestavene        */
-/*						funkce a tabulka symbolu)							  */
-/* Kodovani:            UTF-8                                                 */
-/* Datum:               11.2014                                               */
-/* Predmet:             Formalni jazyky a prekladace (IFJ)                    */
-/* Projekt:             Implementace interpretu jazyka IFJ14                  */
-/* Varianta zadani:     a/2/II                                                */
-/* Titul,Autori, login:         Ing. Natalya Loginova   xlogin00              */
-/*                              Jindrich Dudek          xdudek04              */
-/*                              Norbert Durcansky       xdurca01              */
-/*                              Jan Jusko               xjusko00              */
-/*                              Jiri Dostal             xdosta40              */
-/* ****************************************************************************/
 
 #define FALSE 0
 #define TRUE 1
+#define false 0
+#define true 1
+//slouzi k urceni, zda zaznam je zaznamem funkce ci promenne
+#define VARIABLE 50
+#define FUNCTION 51
+//slouzi k rozeznani datovych/navratovych typu promennych a funkci
+#define INTEGER 100
+#define REAL 101
+#define CHAR 102
+#define STRING 104
 
+typedef union //unie slouzici k ukladani hodnot jednotlivych promennych v zaznamech
+{
+	int i;
+	double d;
+	char c;
+	char *str;
+} DRtype;
+
+struct record
+{ //struktura pro zaznam v hash tabulce
+	int id; //nabyva hodnot FUNCTION a VARIABLE - urcuje, zda je identifikator promenna ci funkce
+	char *key; //ukazatel na identifikator(nazev) funkce/promenne
+	int type; //u funkce je tato promenna nositelem informace o navratovem typu, u promenne informace o datovem typu
+	DRtype value; //u promennych slouzi k ukladani jejich hodnot
+	char *params; //u funkce slouzi k ulozeni informace o typech jejich parametru, u promennych ma hodnotu NULL
+	int defined; //nabyva hodnot true a false - u promenne urcuje, zda je v promenne prirazena nejaka hodnota, u funkce urcuje, zda jiz bylo definovano jeji telo
+	struct record *next;
+};
+
+typedef struct
+{ //struktura pro hash-tabulku
+	unsigned hashtable_size;
+	struct record *ptr[];
+} THash_table;
+
+//prototypy funkci:
+void stringtolower (char *key);
+THash_table *hashtable_init (unsigned size);
+unsigned int hash_function (const char *str, unsigned hashtable_size);
+void hashtable_clear (THash_table *table);
+void hashtable_free(THash_table *table);
+struct record *hashtable_search(THash_table *table, char *key);
+struct record *hashtable_add(THash_table *table, int id, char *key, int type, char *params);
 int length(char *s);
+
+
+
+
 
 char *copy(char *s, int i, int n);
 
